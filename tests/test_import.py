ebfa19a0a0d61d89b15df15e78c9052d29a6e9a3
Jason Garman <jgarman@bit9.com>
2016-05-23 17:29:57 -0400

    adding initial continuous integration testing

---
+++ tests/test_import.py
@@ -0,0 +1,10 @@
+import unittest
+from cbapi import CbEnterpriseProtectionAPI, CbEnterpriseResponseAPI
+
+
+class TestBasicImport(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_import(self):
+        pass
