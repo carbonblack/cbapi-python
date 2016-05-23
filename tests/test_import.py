41f1e75e033c65ca308de9ca317160a6b65effc1
Jason Garman <jgarman@bit9.com>
2016-05-23 17:33:05 -0400

    trigger travisci build

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,5 +1,9 @@
 import unittest
+
+from nose.tools import assert_raises
+
 from cbapi import CbEnterpriseProtectionAPI, CbEnterpriseResponseAPI
+from cbapi.errors import CredentialError
 
 
 class TestBasicImport(unittest.TestCase):
@@ -8,3 +12,9 @@ class TestBasicImport(unittest.TestCase):
 
     def test_import(self):
         pass
+
+    def test_cbep_credential_error(self):
+        assert_raises(CredentialError, CbEnterpriseProtectionAPI)
+
+    def test_cber_credential_error(self):
+        assert_raises(CredentialError, CbEnterpriseProtectionAPI)
