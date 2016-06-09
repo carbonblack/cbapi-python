2d9c4a807dbe10441b906ae010dbfb5ac7eea468
Jason McFarland <jmcfarland@carbonblack.com>
2016-06-09 10:29:25 -0500

    updating tests to store results inside of a sqlite db

--- tests/test_cber.py
+++ tests/test_cber.py
@@ -8,6 +8,7 @@ import requests_cache
 import unittest
 import os
 import sys
+import sqlite3
 
 #import requests.packages.urllib3
 #requests.packages.urllib3.disable_warnings()
@@ -17,17 +18,11 @@ import sys
 # TODO: move this to the sqlite golden cache file
 #
 if sys.version_info >= (3, 0):
-    GOLDEN_CACHE_NAME = os.path.join(os.path.dirname(__file__), "caches/cache.py3")
-    NUMBER_OF_BINARIES = 636
-    NUMBER_OF_PROCESSES = 2274
+    cache_file_name = os.path.join(os.path.dirname(__file__), "caches/cache.py3")
 elif sys.version_info >= (2, 7):
-    GOLDEN_CACHE_NAME = os.path.join(os.path.dirname(__file__), "caches/cache.py27")
-    NUMBER_OF_BINARIES = 636
-    NUMBER_OF_PROCESSES = 2272
+    cache_file_name = os.path.join(os.path.dirname(__file__), "caches/cache.py27")
 elif sys.version_info >= (2, 6):
-    GOLDEN_CACHE_NAME = os.path.join(os.path.dirname(__file__), "caches/cache.py26")
-    NUMBER_OF_BINARIES = 636
-    NUMBER_OF_PROCESSES = 2274
+    cache_file_name = os.path.join(os.path.dirname(__file__), "caches/cache.py26")
 else:
     raise Exception("Unsupported python version")
 
@@ -39,10 +34,9 @@ if config['cache']['use_golden'].lower() == "false":
 else:
     use_golden = True
 
-if use_golden:
-    cache_file_name = GOLDEN_CACHE_NAME
-else:
-    cache_file_name = config['cache']['cache_filename']
+if not use_golden:
+    if 'cache_file_name' in config['cache']:
+        cache_file_name = config['cache']['cache_filename']
     if config['cache']['cache_overwrite'].lower() == 'true':
         overwrite = True
         #
@@ -53,11 +47,39 @@ else:
         except:
             pass
 
+    #
+    # Create our assertion table
+    #
+    con = sqlite3.connect(cache_file_name + ".sqlite")
+    cur = con.cursor()
+    cur.execute('drop table if exists AssertionTest')
+    cur.execute('create table AssertionTest (testname string primary key, testresult string)')
+    con.commit()
+    con.close()
+
+
 #
 # Install the cache
 # NOTE: deny_outbound is set so we don't attempt comms and force using the cache
 #
 requests_cache.install_cache(cache_file_name, allowable_methods=('GET', 'POST'), deny_outbound=use_golden)
+
+
+def insertResult(testname, testresult):
+    con = sqlite3.connect(cache_file_name + ".sqlite")
+    cur = con.cursor()
+    cur.execute('insert into AssertionTest VALUES ("{0}", "{1}")'.format(testname, testresult))
+    con.commit()
+    con.close()
+
+
+def getTestResult(testname):
+    con = sqlite3.connect(cache_file_name + ".sqlite")
+    cur = con.cursor()
+    cur.execute('select testresult from AssertionTest where testname == "{0}"'.format(testname))
+    result = cur.fetchone()
+    con.close()
+    return result
 
 
 class TestCbResponse(unittest.TestCase):
@@ -75,37 +97,29 @@ class TestCbResponse(unittest.TestCase):
     def test_all_binary(self):
         binary_query = c.select(Binary).where('')
         if use_golden:
+            test_result = getTestResult('test_all_binary')[0]
             assert_equal(len(binary_query),
-                         NUMBER_OF_BINARIES,
-                         "Number of Binaries returned should be %d, but received %d" % (
-                         NUMBER_OF_BINARIES, len(binary_query)))
+                         test_result,
+                         "Number of Binaries returned should be {0}, but received {1}".format(
+                             test_result, len(binary_query)))
         else:
-            #
-            # TODO: Save to sqlite database
-            #
-            print(len(binary_query))
+            insertResult('test_all_binary', str(len(binary_query)))
 
     def test_read_binary(self):
         data = c.select(Binary).where('').first().file.read(2)
         if use_golden:
             assert_equal(data, b'MZ')
-        else:
-            #
-            # Save off the returned value into the database
-            #
-            pass
 
     def test_all_process(self):
         process_query = c.select(Process).where('')
         if use_golden:
+            test_result = getTestResult('test_all_process')[0]
             assert_equal(len(process_query),
-                         NUMBER_OF_PROCESSES,
-                         "Number of Processes returned should be %d, but received %d" % (
-                         NUMBER_OF_PROCESSES, len(process_query)))
+                         test_result,
+                         "Number of Processes returned should be {0}, but received {1}".format(
+                             test_result, len(process_query)))
         else:
-            # Save off the returned value into the database
-            print(len(process_query))
-            pass
+            insertResult('test_all_process', str(len(process_query)))
 
 
 
