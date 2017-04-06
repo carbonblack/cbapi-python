042ee4f3a4f0078f878c884251e98634949d3b12
Jason Garman <jgarman@carbonblack.com>
2017-04-06 14:23:56 -0400

    add Event model object to the defense API

--- src/cbapi/defense/models.py
+++ src/cbapi/defense/models.py
@@ -12,3 +12,15 @@ class Device(NewBaseModel):
         if type(obj) == dict and "deviceInfo" in obj:
             return obj["deviceInfo"]
 
+
+class Event(NewBaseModel):
+    urlobject = "/integrationServices/v3/event"
+    primary_key = "eventId"
+
+    def __init__(self, cb, model_unique_id, initial_data=None):
+        super(Event, self).__init__(cb, model_unique_id, initial_data)
+
+    def _parse(self, obj):
+        if type(obj) == dict and "eventInfo" in obj:
+            return obj["eventInfo"]
+
