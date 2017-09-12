249b119ac74aa3eb17b2c8b2024e8bed9ffcc605
Jason Garman <jgarman@carbonblack.com>
2017-09-12 10:43:12 -0400

    change copyright notice for 2017

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'cbapi'
-copyright = u'2016, Carbon Black Developer Network'
+copyright = u'2016-2017, Carbon Black Developer Network'
 author = u'Carbon Black Developer Network'
 
 # The version info for the project you're documenting, acts as replacement for
