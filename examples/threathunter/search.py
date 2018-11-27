044116f1856c4ecb52899902a1f69541b42f46de
William Woodruff <william.woodruff@trailofbits.com>
2018-11-27 11:39:47 -0500

    examples: Update CbTH example

--- examples/threathunter/search.py
+++ examples/threathunter/search.py
@@ -3,8 +3,7 @@
 import sys
 
 from cbapi.example_helpers import build_cli_parser, get_cb_threathunter_object
-from cbapi.psc.threathunter import Process
-from solrq import Q
+from cbapi.psc.threathunter import Process, Tree
 
 
 def main():
@@ -13,13 +12,17 @@ def main():
     args = parser.parse_args()
     cb = get_cb_threathunter_object(args)
 
-    # query = Q(process_name="svchost.exe")
-    # query2 = Q(process_name="conhost.exe")
-    # processes = cb.select(Process).where(query).or_(query2)
-    processes = cb.select(Process).where(process_name="svchost.exe").and_(process_pid=3056)
+    # query processes
+    processes = cb.select(Process).where(process_name="svchost.exe")
 
     for process in processes:
-        print(process)
+        print(process.process_guid)
+
+    # query the process tree
+    children = cb.select(Tree).where(process_guid=processes[0].process_guid)
+
+    for child in children:
+        print(child)
 
 
 if __name__ == "__main__":
