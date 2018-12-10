e3efb6ef3b5a0bce034d447537b936a909af64a6
William Woodruff <william.woodruff@trailofbits.com>
2018-12-10 12:29:00 -0500

    examples: tmp

--- examples/threathunter/search.py
+++ examples/threathunter/search.py
@@ -8,7 +8,11 @@ from solrq import Range, Value
 
 
 def main():
-    parser = build_cli_parser("Search processes (CbTH)")
+    parser = build_cli_parser("Search processes")
+    parser.add_argument("-q", type=str, help="process query", default="process_name:notepad.exe")
+    # parser.add_argument("-n", type=int, help="limit results to N proceeses", default=0)
+    parser.add_argument("-e", type=bool, help="show events for query results", default=False)
+    parser.add_argument("-c", type=bool, help="show children for query results", default=False)
 
     args = parser.parse_args()
     cb = get_cb_threathunter_object(args)
@@ -16,29 +20,36 @@ def main():
     print("Number of queries: {}".format(len(cb.queries())))
     print("API limits: {}".format(cb.limits()))
 
-    processes = cb.select(Process).where(process_name="notepad.exe")
+    processes = cb.select(Process).where(args.q)
 
     print("Number of processes: {}".format(len(processes)))
 
-    process = processes[0]
+    for process in processes:
+        print(process)
 
-    events = process.events(event_type=Value("*", safe=True))
-    # alternatively:
-    # events = cb.select(Events).where(process_guid=process.process_guid)
+    # process = processes[0]
 
-    print("Number of events: {}".format(len(events)))
+    # events = process.events(event_type=Value("*", safe=True))
 
-    # tree = process.tree()
-    # alternatively:
-    # tree = cb.select(Tree).where(process_guid=process.process_guid)
+    # for event in events:
+    #     print(event.event_guid)
 
-    # get the children directly:
-    children = process.children()
+    # # alternatively:
+    # # events = cb.select(Events).where(process_guid=process.process_guid)
 
-    print("Number of children: {}".format(len(children)))
+    # print("Number of events: {}".format(len(events)))
 
-    for child in children:
-        print(child)
+    # # tree = process.tree()
+    # # alternatively:
+    # # tree = cb.select(Tree).where(process_guid=process.process_guid)
+
+    # # get the children directly:
+    # children = process.children()
+
+    # print("Number of children: {}".format(len(children)))
+
+    # for child in children:
+    #     print(child)
 
 
 if __name__ == "__main__":
