60444fd5970f64d5f8f937a36ce052961f8cb095
William Woodruff <william.woodruff@trailofbits.com>
2018-12-19 10:05:21 -0500

    threathunter/models: Begin filling in docs

--- src/cbapi/psc/threathunter/models.py
+++ src/cbapi/psc/threathunter/models.py
@@ -9,11 +9,16 @@ log = logging.getLogger(__name__)
 
 
 class UnrefreshableModel(NewBaseModel):
+    """Represents a model that can't be refreshed, i.e. for which ``reset()``
+    is not a valid operation.
+    """
     def refresh(self):
         raise ApiError("refresh() called on an unrefreshable model")
 
 
 class Process(UnrefreshableModel):
+    """Represents a process retrieved by one of the CbTH endpoints.
+    """
     default_sort = 'last_update desc'
     primary_key = "process_guid"
     validation_url = "/pscr/query/v1/validate"
@@ -28,16 +33,46 @@ class Process(UnrefreshableModel):
                                       force_init=force_init, full_doc=full_doc)
 
     def events(self, **kwargs):
+        """Returns a query for *Events* associated with this process's process GUID.
+
+        :param kwargs: Arguments to filter the event query with.
+        :return: Returns a :py:class:`threathunter.rest_api.Query` object with the appropriate search parameters for events
+        :rtype: :py:class:`threathunter.rest_api.Query`
+
+        Example::
+
+        >>> [print(event) for event in process.events()]
+        >>> [print(event) for event in process.events(event_type="modload")]
+        """
         return self._cb.select(Events).where(process_guid=self.process_guid).and_(**kwargs)
 
     def tree(self):
+        """Returns a *Tree* of children (and possibly siblings) associated with this process.
+
+        :return: Returns a :py:class:`Tree` object
+        :rtype: :py:class:`Tree`
+
+        Example:
+
+        >>> tree = process.tree()
+        """
         data = self._cb.select(Tree).where(process_guid=self.process_guid).all()
         return Tree(self._cb, initial_data=data)
 
     def parents(self):
+        """Returns a query for parent processes associated with this process.
+
+        :return: Returns a :py:class:`threathunter.rest_api.Query` object with the appropriate search parameters for parent processes
+        :rtype: :py:class:`threathunter.rest_api.Query`
+        """
         return self._cb.select(Process).where(process_guid=self.parent_guid)
 
     def children(self):
+        """Returns a list of child processes for this process.
+
+        :return: Returns a list of process objects
+        :rtype: list of :py:class:`Process`
+        """
         return self.tree().children()
 
     def siblings(self):
@@ -51,9 +86,19 @@ class Process(UnrefreshableModel):
     # TODO(ww): Is the order of the MD5/SHA256 in process_hash guaranteed?
     # Should we check the lengths instead?
     def process_md5(self):
+        """Returns a string representation of the MD5 hash for this process.
+
+        :return: A string representation of the process's MD5.
+        :rtype: str
+        """
         return self.process_hash[0]
 
     def process_sha256(self):
+        """Returns a string representation of the SHA256 hash for this process.
+
+        :return: A string representation of the process's SHA256.
+        :rtype: str
+        """
         return self.process_hash[1]
 
     def __repr__(self):
@@ -78,6 +123,8 @@ class Events(UnrefreshableModel):
 
 
 class Tree(UnrefreshableModel):
+    """Represents a process tree.
+    """
     urlobject = '/pscr/query/v2/tree'
     primary_key = 'process_guid'
 
