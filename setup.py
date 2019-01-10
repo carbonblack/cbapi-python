41657c11826e357d03d2eaa5b59ca7fdc284e9e1
Sean McFeely <mcfeelynaes@gmail.com>
2019-01-10 09:20:17 -0500

    added threathunter to pip setup (#128)

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if sys.version_info < (3, 0):
 
 setup(
     name='cbapi',
-    version='1.3.7',
+    version='1.3.8',
     url='https://github.com/carbonblack/cbapi-python',
     license='MIT',
     author='Carbon Black',
