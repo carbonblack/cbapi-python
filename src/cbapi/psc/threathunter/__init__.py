55e3cde15fc913813f07ad89c7ced3270cd02d61
William Woodruff <william.woodruff@trailofbits.com>
2019-01-11 17:33:04 -0500

    threathunter: Restrict Feed API to PY3
    Only for the time being.

--- src/cbapi/psc/threathunter/__init__.py
+++ src/cbapi/psc/threathunter/__init__.py
@@ -2,8 +2,11 @@
 
 from __future__ import absolute_import
 
+from cbapi.six import PY3
+
 from .rest_api import CbThreatHunterAPI
 from cbapi.psc.threathunter.models import Process, Event, Tree
 from cbapi.psc.threathunter.query import QueryBuilder
 
-from .feed_api import CbThreatHunterFeedAPI
+if PY3:
+    from .feed_api import CbThreatHunterFeedAPI
