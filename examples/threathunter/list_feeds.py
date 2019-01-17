56f4263b297baed5bba1376091647e361a848cc6
William Woodruff <william.woodruff@trailofbits.com>
2019-01-17 17:10:06 -0500

    examples: Add CbTH Feed API listing example

---
+++ examples/threathunter/list_feeds.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+from cbapi.example_helpers import build_cli_parser, get_cb_threathunter_feed_object
+
+
+def main():
+    parser = build_cli_parser("List CbTH feeds")
+    parser.add_argument("-p", help="show public feeds in addition to private ones", action="store_true", default=False)
+
+    args = parser.parse_args()
+    cb = get_cb_threathunter_feed_object(args)
+
+    for feed in cb.feeds(include_public=args.p):
+        print(feed)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
