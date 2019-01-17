27f3048991e02180929eca65ecb062f5ac0b7586
William Woodruff <william.woodruff@trailofbits.com>
2019-01-17 17:38:42 -0500

    examples: Add arguments to feed creation ex

--- examples/threathunter/create_feed.py
+++ examples/threathunter/create_feed.py
@@ -7,6 +7,12 @@ from cbapi.example_helpers import build_cli_parser, get_cb_threathunter_feed_obj
 
 def main():
     parser = build_cli_parser("Create a CbTH feed")
+    parser.add_argument("--name", type=str, help="Feed name", default=None)
+    parser.add_argument("--owner", type=str, help="Feed owner", default=None)
+    parser.add_argument("--url", type=str, help="Feed provider url", default="https://example.com")
+    parser.add_argument("--summary", type=str, help="Feed summary", default=None)
+    parser.add_argument("--category", type=str, help="Feed category", default="Partner")
+    parser.add_argument("--access", type=str, help="Feed access scope", default="private")
 
     args = parser.parse_args()
     cb = get_cb_threathunter_feed_object(args)
