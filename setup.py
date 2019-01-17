076f4797af84462cf2015b175f08b3ac0d9a2744
Sharan Multani <smultani@carbonblack.com>
2019-01-17 13:36:02 -0500

    v1.4.0
    v1.4.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if sys.version_info < (3, 0):
 
 setup(
     name='cbapi',
-    version='1.3.9',
+    version='1.4.0',
     url='https://github.com/carbonblack/cbapi-python',
     license='MIT',
     author='Carbon Black',
