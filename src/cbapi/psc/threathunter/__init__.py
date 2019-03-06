aa4828d86f5d71aea5fd9938774e7409108d9299
William Woodruff <william@trailofbits.com>
2019-03-06 11:16:33 -0500

    threathunter: Remove unused PY3 import

--- src/cbapi/psc/threathunter/__init__.py
+++ src/cbapi/psc/threathunter/__init__.py
@@ -2,8 +2,6 @@
 
 from __future__ import absolute_import
 
-from cbapi.six import PY3
-
 from .rest_api import CbThreatHunterAPI
 from cbapi.psc.threathunter.models import Process, Event, Tree, Feed, Report, IOCs, IOC_V2, Watchlist
 from cbapi.psc.threathunter.query import QueryBuilder
