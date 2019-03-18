46c17be20682a1b23737a3d758d161ba368fcb87
Zachary Estep <zestep@carbonblack.com>
2019-03-18 15:08:24 -0400

    updates

--- src/cbapi/psc/defense/rest_api.py
+++ src/cbapi/psc/defense/rest_api.py
@@ -162,6 +162,10 @@ class Query(PaginatedQuery):
 
             results = result.get('results', [])
 
+            if results is None:
+                log.debug("!!!RESULTS ARE NONE!!!")
+                break
+
             for item in results:
                 yield item
                 current += 1
@@ -174,10 +178,7 @@ class Query(PaginatedQuery):
 
             if current >= self._total_results:
                 break
-            #Terminate cleanly from maximum 100k
-            if current >= 100000:
-                log.info("WARNING: Hit Max Search Result Set Limit(100k)")
-                break
+
             if not results:
                 log.debug("server reported total_results overestimated the number of results for this query by {0}"
                           .format(self._total_results - current))
