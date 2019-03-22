ec7de784632a8cbc4aa65d8eb11702308250cb3b
William Woodruff <william@trailofbits.com>
2019-03-22 11:52:57 -0400

    examples: Update process_query (CbTH)

--- examples/threathunter/process_query.py
+++ examples/threathunter/process_query.py
@@ -3,13 +3,14 @@
 import sys
 
 from cbapi.example_helpers import build_cli_parser, get_cb_threathunter_object
-from cbapi.psc.threathunter import Process
+from cbapi.psc.threathunter import Process, Binary
 
 
 def main():
     parser = build_cli_parser("Query processes")
     parser.add_argument("-q", type=str, help="process query", default="process_name:notepad.exe")
     parser.add_argument("-n", type=int, help="only output N processes", default=None)
+    parser.add_argument("-b", action="store_true", help="show binary information", default=False)
 
     args = parser.parse_args()
     cb = get_cb_threathunter_object(args)
@@ -24,6 +25,10 @@ def main():
         print("\tPIDs: {}".format(process.process_pids))
         print("\tSHA256: {}".format(process.process_sha256))
         print("\tGUID: {}".format(process.process_guid))
+
+        if args.b:
+            binary = cb.select(Binary, process.process_sha256)
+            print("\t{}".format(binary))
 
 if __name__ == "__main__":
     sys.exit(main())
