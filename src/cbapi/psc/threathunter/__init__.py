0ca6dc5ec5cd904a3b55773dea6b52b2088c8cdd
William Woodruff <william@trailofbits.com>
2019-04-01 13:46:39 -0400

    threathunter: Add Downloads model to init

--- src/cbapi/psc/threathunter/__init__.py
+++ src/cbapi/psc/threathunter/__init__.py
@@ -4,6 +4,6 @@ from __future__ import absolute_import
 
 from .rest_api import CbThreatHunterAPI
 from cbapi.psc.threathunter.models import (
-    Process, Event, Tree, Feed, Report, IOC, IOC_V2, Watchlist, Binary
+    Process, Event, Tree, Feed, Report, IOC, IOC_V2, Watchlist, Binary, Downloads
 )
 from cbapi.psc.threathunter.query import QueryBuilder
