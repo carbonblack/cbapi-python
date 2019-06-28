40bfeed6f13037e498ed1c3f949a3ab6c66fc6dc
William Woodruff <william@trailofbits.com>
2019-06-28 16:18:26 -0400

    examples: Add run_search LiveQuery example

---
+++ examples/livequery/run_search.py
@@ -0,0 +1,26 @@
+import sys
+
+from cbapi.example_helpers import build_cli_parser, get_cb_livequery_object
+from cbapi.psc.livequery.models import Result
+
+
+def main():
+    parser = build_cli_parser("Search the results of a LiveQuery run")
+    parser.add_argument("-i", "--id", type=str, required=True, help="Run ID")
+    parser.add_argument("-q", "--query", type=str, required=False, help="search query")
+
+    # TODO(ww): Flags for criteria, sort_by
+
+    args = parser.parse_args()
+    cb = get_cb_livequery_object(args)
+
+    results = cb.select(Result).run_id(args.id)
+    if args.query:
+        results = results.where(args.query)
+
+    for result in results:
+        print(result)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
