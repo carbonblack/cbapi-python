49f97886f08a75438ed3cd650fb3cab87ae422fa
llyon-cb <llyon@carbonblack.com>
2019-08-05 12:23:34 -0600

    add livequery to packages in setup

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ packages = [
     'cbapi.cache',
     'cbapi.psc',
     'cbapi.psc.defense',
-    'cbapi.psc.threathunter'
+    'cbapi.psc.threathunter',
+    'cbapi.psc.livequery'
 ]
 
 install_requires=[
@@ -36,7 +37,7 @@ if sys.version_info < (3, 0):
 
 setup(
     name='cbapi',
-    version='1.5.1',
+    version='1.5.0',
     url='https://github.com/carbonblack/cbapi-python',
     license='MIT',
     author='Carbon Black',
