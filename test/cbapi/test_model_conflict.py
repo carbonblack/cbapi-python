e1a5e6a97ee052af0d18a0b667455b7fe1acd9fb
Amy Bowersox <abowersox@carbonblack.com>
2019-09-17 12:08:39 -0600

    Amy's experiment in moving modules between directories/packages

---
+++ test/cbapi/test_model_conflict.py
@@ -0,0 +1,21 @@
+import pytest
+from cbapi.psc.models import Policy
+from cbapi.psc.defense.models import Policy as PolicyOld
+from cbapi.example_helpers import get_cb_defense_object
+
+class TempArgs:
+    pass
+
+def test_model_conflict():
+    args = TempArgs()
+    args.verbose = False
+    args.cburl = "https://localhost.example.com"
+    args.apitoken = "foo"
+    args.no_ssl_verify = True
+    apiobj = get_cb_defense_object(args)
+    mod1 = Policy(apiobj)
+    mod2 = PolicyOld(apiobj)
+    mod1.do_funky_things()
+    with pytest.raises(AttributeError):
+        mod2.do_funky_things()
+    
