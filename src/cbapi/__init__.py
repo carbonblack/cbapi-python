800954a314b89104d72b2c3d39c81c0ad52ffb2e
Amy Bowersox <abowersox@carbonblack.com>
2019-09-23 14:54:19 -0600

    adding the API object to the top level __init__.py as well
    request-checks: true

--- src/cbapi/__init__.py
+++ src/cbapi/__init__.py
@@ -11,6 +11,7 @@ __version__ = '1.3.6'
 # New API as of cbapi 0.9.0
 from cbapi.response.rest_api import CbEnterpriseResponseAPI, CbResponseAPI
 from cbapi.protection.rest_api import CbEnterpriseProtectionAPI, CbProtectionAPI
+from cbapi.psc import CbPSCBaseAPI
 from cbapi.psc.defense import CbDefenseAPI
 from cbapi.psc.threathunter import CbThreatHunterAPI
 from cbapi.psc.livequery import CbLiveQueryAPI
