36369f6cd10220a63836ce26f4bb67a6b646bc70
Amy Bowersox <abowersox@carbonblack.com>
2019-09-23 14:41:03 -0600

    changed the export definitions in PSC package
    request-checks: true

--- src/cbapi/psc/__init__.py
+++ src/cbapi/psc/__init__.py
@@ -2,5 +2,5 @@
 
 from __future__ import absolute_import
 
-from .rest_api import CbPSCBaseAPI
-from cbapi.psc.models import Device as DeviceV6
+from cbapi.psc.rest_api import CbPSCBaseAPI
+from cbapi.psc.models import Device
