31dbcedcd8b16a16b3666cb3c0d9281e70351eaa
Rafael Lukas Maers <rafael@mnemonic.no>
2019-10-02 08:30:31 +0200

    Fix response.models.Process.min/max_last_update

--- src/cbapi/response/models.py
+++ src/cbapi/response/models.py
@@ -3170,19 +3170,20 @@ class Process(TaggedModel):
         """
         return convert_from_solr(self.get('last_update', -1))
 
+
     @property
     def min_last_update(self):
         """
         Returns a pretty version of the earliest event in this process segment
         """
-        return convert_from_solr(self.get('last_update', -1))
+        return convert_from_solr(self.get('min_last_update', -1))
 
     @property
     def max_last_update(self):
         """
         Returns a pretty version of the latest event in this process segment
         """
-        return convert_from_solr(self.get('last_update', -1))
+        return convert_from_solr(self.get('max_last_update', -1))
 
     @property
     def last_server_update(self):
