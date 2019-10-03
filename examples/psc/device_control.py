c38e69ebd67c6d43d8230f1ab9d08b8d23a73b6e
Amy Bowersox <abowersox@carbonblack.com>
2019-10-03 12:47:35 -0600

    some minor glitch cleanup in device_control, and adding logging to download_device_list (this is increasingly looking like a server bug)

--- examples/psc/device_control.py
+++ examples/psc/device_control.py
@@ -14,30 +14,30 @@ def toggle_value(args):
 def main():
     parser = build_cli_parser("Send control messages to device")
     parser.add_argument("-d", "--device_id", type=int, required=True, help="The ID of the device to be controlled")
-    subparsers = parser.add_subparsers(required=True, dest="command", help="Device command help")
+    subparsers = parser.add_subparsers(dest="command", help="Device command help")
     
     bgscan_p = subparsers.add_parser("background_scan", help="Set background scanning status")
     toggle = bgscan_p.add_mutually_exclusive_group(required=True)
-    group.add_argument("--on", action="store_true", help="Turn background scanning on")
-    group.add_argument("--off", action="store_true", help="Turn background scanning off")    
+    toggle.add_argument("--on", action="store_true", help="Turn background scanning on")
+    toggle.add_argument("--off", action="store_true", help="Turn background scanning off")    
     
     bypass_p = subparsers.add_parser("bypass", help="Set bypass mode")
     toggle = bypass_p.add_mutually_exclusive_group(required=True)
-    group.add_argument("--on", action="store_true", help="Enable bypass mode")
-    group.add_argument("--off", action="store_true", help="Disable bypass mode")
+    toggle.add_argument("--on", action="store_true", help="Enable bypass mode")
+    toggle.add_argument("--off", action="store_true", help="Disable bypass mode")
     
     subparsers.add_parser("delete", help="Delete sensor")
     subparsers.add_parser("uninstall", help="Uninstall sensor")
     
     quarantine_p = subparsers.add_parser("quarantine", help="Set quarantine mode")
     toggle = quarantine_p.add_mutually_exclusive_group(required=True)
-    group.add_argument("--on", action="store_true", help="Enable quarantine mode")
-    group.add_argument("--off", action="store_true", help="Disable quarantine mode")
+    toggle.add_argument("--on", action="store_true", help="Enable quarantine mode")
+    toggle.add_argument("--off", action="store_true", help="Disable quarantine mode")
     
     policy_p = subparsers.add_parser("policy", help="Update policy for node")
     policy_p.add_argument("-p", "--policy_id", type=int, required=True, help="New policy ID to set for node")
     
-    sensorv_p = subparses_parser("sensor_version", help="Update sensor version for node")
+    sensorv_p = subparsers.add_parser("sensor_version", help="Update sensor version for node")
     sensorv_p.add_argument("-o", "--os", required=True, help="Operating system for sensor")
     sensorv_p.add_argument("-V", "--version", required=True, help="Version number of sensor")
     
@@ -45,24 +45,26 @@ def main():
     cb = get_cb_psc_object(args)
     dev = cb.get_device(args.device_id)
     
-    if args.command == "background_scan":
-        dev.background_scan(toggle_value(args))
-    elif args.command == "bypass":
-        dev.bypass(toggle_value(args))
-    elif args.command == "delete":
-        dev.delete_sensor()
-    elif args.command == "uninstall":
-        dev.uninstall_sensor()
-    elif args.command == "quarantine":
-        dev.quarantine(toggle_value(args))
-    elif args.command == "policy":
-        dev.update_policy(args.policy_id)
-    elif args.command == "sensor_version":
-        dev.update_sensor_version({args.os: args.version})
+    if args.command:
+        if args.command == "background_scan":
+            dev.background_scan(toggle_value(args))
+        elif args.command == "bypass":
+            dev.bypass(toggle_value(args))
+        elif args.command == "delete":
+            dev.delete_sensor()
+        elif args.command == "uninstall":
+            dev.uninstall_sensor()
+        elif args.command == "quarantine":
+            dev.quarantine(toggle_value(args))
+        elif args.command == "policy":
+            dev.update_policy(args.policy_id)
+        elif args.command == "sensor_version":
+            dev.update_sensor_version({args.os: args.version})
+        else:
+            raise NotImplementedError("Unknown command")
+        print("OK")
     else:
-        raise NotImplementedError("Unknown command")
-    print("OK")
-    
+        print(dev)
     
 if __name__ == "__main__":
     sys.exit(main())
