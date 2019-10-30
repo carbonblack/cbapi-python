769e1aabbd89816b42cbce0fec81e94174669f13
Amy Bowersox <abowersox@carbonblack.com>
2019-10-30 13:11:01 -0600

    rework device tests to ensure that we start from a correct standpoint

--- test/cbapi/psc/test_models.py
+++ test/cbapi/psc/test_models.py
@@ -13,18 +13,35 @@ class MockScheduler:
         self.was_called = True
         return { "itworks": True }
 
-def test_Device_lr_session():
+def test_Device_lr_session(monkeypatch):
+    _device_data = {"id": 6023}
+
+    def mock_get_object(url, parms=None, default=None):
+        nonlocal _device_data
+        assert url == "/appservices/v6/orgs/Z100/devices/6023"
+        return _device_data
+
     api = CbPSCBaseAPI(url="https://example.com", token="ABCD/1234",
                        org_key="Z100", ssl_verify=True)
     sked = MockScheduler(6023)
     api._lr_scheduler = sked
-    dev = Device(api, 6023)
+    monkeypatch.setattr(api, "get_object", mock_get_object)
+    monkeypatch.setattr(api, "post_object", ConnectionMocks.get("POST"))
+    monkeypatch.setattr(api, "put_object", ConnectionMocks.get("PUT"))
+    monkeypatch.setattr(api, "delete_object", ConnectionMocks.get("DELETE"))
+    dev = Device(api, 6023, _device_data)
     sess = dev.lr_session()
     assert sess["itworks"]
     assert sked.was_called
     
 def test_Device_background_scan(monkeypatch):
+    _device_data = {"id": 6023}
     _was_called = False
+    
+    def mock_get_object(url, parms=None, default=None):
+        nonlocal _device_data
+        assert url == "/appservices/v6/orgs/Z100/devices/6023"
+        return _device_data
     
     def mock_post_object(url, body, **kwargs):
         nonlocal _was_called
@@ -38,16 +55,22 @@ def test_Device_background_scan(monkeypatch):
         
     api = CbPSCBaseAPI(url="https://example.com", token="ABCD/1234",
                        org_key="Z100", ssl_verify=True)
-    dev = Device(api, 6023)
-    monkeypatch.setattr(api, "get_object", ConnectionMocks.get("GET"))
+    monkeypatch.setattr(api, "get_object", mock_get_object)
     monkeypatch.setattr(api, "post_object", mock_post_object)
     monkeypatch.setattr(api, "put_object", ConnectionMocks.get("PUT"))
     monkeypatch.setattr(api, "delete_object", ConnectionMocks.get("DELETE"))
+    dev = Device(api, 6023, _device_data)
     dev.background_scan(True)
     assert _was_called
     
 def test_Device_bypass(monkeypatch):
+    _device_data = {"id": 6023}
     _was_called = False
+    
+    def mock_get_object(url, parms=None, default=None):
+        nonlocal _device_data
+        assert url == "/appservices/v6/orgs/Z100/devices/6023"
+        return _device_data
     
     def mock_post_object(url, body, **kwargs):
         nonlocal _was_called
@@ -61,16 +84,22 @@ def test_Device_bypass(monkeypatch):
 
     api = CbPSCBaseAPI(url="https://example.com", token="ABCD/1234",
                        org_key="Z100", ssl_verify=True)
-    dev = Device(api, 6023)
-    monkeypatch.setattr(api, "get_object", ConnectionMocks.get("GET"))
+    monkeypatch.setattr(api, "get_object", mock_get_object)
     monkeypatch.setattr(api, "post_object", mock_post_object)
     monkeypatch.setattr(api, "put_object", ConnectionMocks.get("PUT"))
     monkeypatch.setattr(api, "delete_object", ConnectionMocks.get("DELETE"))
+    dev = Device(api, 6023, _device_data)
     dev.bypass(False)
     assert _was_called
     
 def test_Device_delete_sensor(monkeypatch):
+    _device_data = {"id": 6023}
     _was_called = False
+    
+    def mock_get_object(url, parms=None, default=None):
+        nonlocal _device_data
+        assert url == "/appservices/v6/orgs/Z100/devices/6023"
+        return _device_data
     
     def mock_post_object(url, body, **kwargs):
         nonlocal _was_called
@@ -82,16 +111,22 @@ def test_Device_delete_sensor(monkeypatch):
 
     api = CbPSCBaseAPI(url="https://example.com", token="ABCD/1234",
                        org_key="Z100", ssl_verify=True)
-    dev = Device(api, 6023)
-    monkeypatch.setattr(api, "get_object", ConnectionMocks.get("GET"))
+    monkeypatch.setattr(api, "get_object", mock_get_object)
     monkeypatch.setattr(api, "post_object", mock_post_object)
     monkeypatch.setattr(api, "put_object", ConnectionMocks.get("PUT"))
     monkeypatch.setattr(api, "delete_object", ConnectionMocks.get("DELETE"))
+    dev = Device(api, 6023, _device_data)
     dev.delete_sensor()
     assert _was_called
 
 def test_Device_uninstall_sensor(monkeypatch):
+    _device_data = {"id": 6023}
     _was_called = False
+    
+    def mock_get_object(url, parms=None, default=None):
+        nonlocal _device_data
+        assert url == "/appservices/v6/orgs/Z100/devices/6023"
+        return _device_data
     
     def mock_post_object(url, body, **kwargs):
         nonlocal _was_called
@@ -103,16 +138,22 @@ def test_Device_uninstall_sensor(monkeypatch):
 
     api = CbPSCBaseAPI(url="https://example.com", token="ABCD/1234",
                        org_key="Z100", ssl_verify=True)
-    dev = Device(api, 6023)
-    monkeypatch.setattr(api, "get_object", ConnectionMocks.get("GET"))
+    monkeypatch.setattr(api, "get_object", mock_get_object)
     monkeypatch.setattr(api, "post_object", mock_post_object)
     monkeypatch.setattr(api, "put_object", ConnectionMocks.get("PUT"))
     monkeypatch.setattr(api, "delete_object", ConnectionMocks.get("DELETE"))
+    dev = Device(api, 6023, _device_data)
     dev.uninstall_sensor()
     assert _was_called
         
 def test_Device_quarantine(monkeypatch):
+    _device_data = {"id": 6023}
     _was_called = False
+    
+    def mock_get_object(url, parms=None, default=None):
+        nonlocal _device_data
+        assert url == "/appservices/v6/orgs/Z100/devices/6023"
+        return _device_data
     
     def mock_post_object(url, body, **kwargs):
         nonlocal _was_called
@@ -126,16 +167,22 @@ def test_Device_quarantine(monkeypatch):
         
     api = CbPSCBaseAPI(url="https://example.com", token="ABCD/1234",
                        org_key="Z100", ssl_verify=True)
-    dev = Device(api, 6023)
-    monkeypatch.setattr(api, "get_object", ConnectionMocks.get("GET"))
+    monkeypatch.setattr(api, "get_object", mock_get_object)
     monkeypatch.setattr(api, "post_object", mock_post_object)
     monkeypatch.setattr(api, "put_object", ConnectionMocks.get("PUT"))
     monkeypatch.setattr(api, "delete_object", ConnectionMocks.get("DELETE"))
+    dev = Device(api, 6023, _device_data)
     dev.quarantine(True)
     assert _was_called
 
 def test_Device_update_policy(monkeypatch):
+    _device_data = {"id": 6023}
     _was_called = False
+    
+    def mock_get_object(url, parms=None, default=None):
+        nonlocal _device_data
+        assert url == "/appservices/v6/orgs/Z100/devices/6023"
+        return _device_data
     
     def mock_post_object(url, body, **kwargs):
         nonlocal _was_called
@@ -149,16 +196,22 @@ def test_Device_update_policy(monkeypatch):
         
     api = CbPSCBaseAPI(url="https://example.com", token="ABCD/1234",
                        org_key="Z100", ssl_verify=True)
-    dev = Device(api, 6023)
-    monkeypatch.setattr(api, "get_object", ConnectionMocks.get("GET"))
+    monkeypatch.setattr(api, "get_object", mock_get_object)
     monkeypatch.setattr(api, "post_object", mock_post_object)
     monkeypatch.setattr(api, "put_object", ConnectionMocks.get("PUT"))
     monkeypatch.setattr(api, "delete_object", ConnectionMocks.get("DELETE"))
+    dev = Device(api, 6023, _device_data)
     dev.update_policy(8675309)
     assert _was_called
 
 def test_Device_update_sensor_version(monkeypatch):
+    _device_data = {"id": 6023}
     _was_called = False
+    
+    def mock_get_object(url, parms=None, default=None):
+        nonlocal _device_data
+        assert url == "/appservices/v6/orgs/Z100/devices/6023"
+        return _device_data
     
     def mock_post_object(url, body, **kwargs):
         nonlocal _was_called
@@ -173,11 +226,11 @@ def test_Device_update_sensor_version(monkeypatch):
         
     api = CbPSCBaseAPI(url="https://example.com", token="ABCD/1234",
                        org_key="Z100", ssl_verify=True)
-    dev = Device(api, 6023)
-    monkeypatch.setattr(api, "get_object", ConnectionMocks.get("GET"))
+    monkeypatch.setattr(api, "get_object", mock_get_object)
     monkeypatch.setattr(api, "post_object", mock_post_object)
     monkeypatch.setattr(api, "put_object", ConnectionMocks.get("PUT"))
     monkeypatch.setattr(api, "delete_object", ConnectionMocks.get("DELETE"))
+    dev = Device(api, 6023, _device_data)
     dev.update_sensor_version({"RHEL": "2.3.4.5"})
     assert _was_called
     
