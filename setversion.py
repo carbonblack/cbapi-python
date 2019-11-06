079b11437e98a7805078ff5a950de9c33775eb32
Amy Bowersox <abowersox@carbonblack.com>
2019-11-06 15:21:27 -0700

    the basics of the script are all now in place, awaiting some refinements

--- setversion.py
+++ setversion.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+from datetime import date
 
 def readme_rewriter(line, versionnum, ctxt):
     expr = ctxt.get("readme_expr", None)
@@ -19,7 +20,52 @@ def changelog_rewriter(line, versionnum, ctxt):
         expr = re.compile(r"^\.\. top-of-changelog")
         ctxt["changelog_expr"] = expr
     if expr.match(line):
-        pass
+        datestr = date.today().strftime("%B %d, %Y")
+        cl1 = "CbAPI {0} - Released {1}".format(versionnum, datestr)
+        cl2 = "".ljust(len(cl1), "-")
+        updates = "\n\nUpdates\n\n.. (add your updates here)\n"
+        return line + "\n" + cl1 + "\n" + cl2 + updates
+    return None
+
+
+def doc_conf_rewriter(line, versionnum, ctxt):
+    vexpr = ctxt.get("doc_version_expr", None)
+    if not vexpr:
+        vexpr = re.compile(r"^version = ")
+        ctxt["doc_version_expr"] = vexpr
+    rexpr = ctxt.get("doc_release_expr", None)
+    if not rexpr:
+        rexpr = re.compile(r"^release = ")
+        ctxt["doc_release_expr"] = rexpr
+    if vexpr.match(line):
+        t = re.match(r"^(\d+\.\d+)\.", versionnum)
+        vn = versionnum
+        if t:
+            vn = t.group(1)
+        return "version = u'{0}'\n".format(vn)
+    if rexpr.match(line):
+        return "release = u'{0}'\n".format(versionnum)
+    return None
+
+
+def setup_rewriter(line, versionnum, ctxt):
+    expr = ctxt.get("setup_expr", None)
+    if not expr:
+        expr = re.compile(r"^(\s*)version=")
+        ctxt["setup_expr"] = expr
+    m = expr.match(line)
+    if m:
+        return "{0}version='{1}'\n".format(m.group(1), versionnum)
+    return None
+
+
+def init_rewriter(line, versionnum, ctxt):
+    expr = ctxt.get("init_expr", None)
+    if not expr:
+        expr = re.compile(r"^__version__ = ")
+        ctxt["init_expr"] = expr
+    if expr.match(line):
+        return "__version__ = '{0}'\n".format(versionnum)
     return None
     
 
@@ -47,6 +93,10 @@ def main():
     version = sys.argv[1]
     ctxt = {}
     rewrite_file("README.md", readme_rewriter, version, ctxt)
+    rewrite_file("docs/changelog.rst", changelog_rewriter, version, ctxt)
+    rewrite_file("docs/conf.py", doc_conf_rewriter, version, ctxt)
+    rewrite_file("setup.py", setup_rewriter, version, ctxt)
+    rewrite_file("src/cbapi/__init__.py", init_rewriter, version, ctxt)
     return 0
     
 if __name__ == "__main__":
