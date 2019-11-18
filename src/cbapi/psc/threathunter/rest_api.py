aa71ec197ab9277748a5398baf18b30a62891a69
Becca Vasil <bvasil@bvasil-mac.bit9.local>
2019-11-18 11:41:41 -0700

    changed PSC to Carbon Black Cloud for rebranding to VMware

--- src/cbapi/psc/threathunter/rest_api.py
+++ src/cbapi/psc/threathunter/rest_api.py
@@ -8,7 +8,7 @@ log = logging.getLogger(__name__)
 
 
 class CbThreatHunterAPI(BaseAPI):
-    """The main entry point into the Cb ThreatHunter PSC API.
+    """The main entry point into the Carbon Black Cloud ThreatHunter API.
 
     :param str profile: (optional) Use the credentials in the named profile when connecting to the Carbon Black server.
         Uses the profile named 'default' when not specified.
