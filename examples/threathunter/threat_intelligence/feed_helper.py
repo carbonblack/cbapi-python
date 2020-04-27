21c52086579c18c8db31ad7a86158755da6d35b6
Luke <llyon@carbonblack.com>
2020-04-27 10:42:57 -0700

    raise ValueError on wrong config start_date input type

--- examples/threathunter/threat_intelligence/feed_helper.py
+++ examples/threathunter/threat_intelligence/feed_helper.py
@@ -4,7 +4,7 @@ This is tied to the `start_date` and `size_of_request_in_minutes` configuration 
 """
 
 from datetime import datetime, timedelta, timezone
-
+import logging
 log = logging.getLogger(__name__)
 
 
@@ -13,10 +13,11 @@ class FeedHelper():
         self.size_of_request_in_minutes = size_of_request_in_minutes
         if isinstance(start_date, datetime):
             self.start_date = start_date.replace(tzinfo=timezone.utc)
-        elif isinstance(start_date, string):
+        elif isinstance(start_date, str):
             self.start_date = datetime.strptime(start_date, "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
         else:
             log.error(f"Start_date must be a string or datetime object. Received a start_time config value with unsupported type: {type(start_date)}")
+            raise ValueError
         self.end_date = self.start_date + \
             timedelta(minutes=self.size_of_request_in_minutes)
         self.now = datetime.utcnow().replace(tzinfo=timezone.utc)
