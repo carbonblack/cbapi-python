233fe172f70d9b59f59972d9a5a0b71fe8f4d210
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2020-05-08 13:30:39 +0000

    Import ABC from collections.abc for Python 3 compatibility.

--- tests/requests_cache/backends/storage/dbdict.py
+++ tests/requests_cache/backends/storage/dbdict.py
@@ -6,7 +6,6 @@
 
     Dictionary-like objects for saving large data sets to `sqlite` database
 """
-from collections import MutableMapping
 import sqlite3 as sqlite
 from contextlib import contextmanager
 try:
@@ -17,6 +16,10 @@ try:
     import cPickle as pickle
 except ImportError:
     import pickle
+try:
+    from collections.abc import MutableMapping
+except ImportError:
+    from collections import MutableMapping
 
 from requests_cache.compat import bytes
 
