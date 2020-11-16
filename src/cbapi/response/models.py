0fb652296427e48cb16ed67423a92eb9c100bc0e
Luke Lyon <52218532+llyon-cb@users.noreply.github.com>
2020-11-16 16:15:19 -0700

    Update Response Models (#279)
    Add Alert.set_ignored(), Alert.assign(), and Alert.change_status()
    Co-authored-by: Jared Fagel <jfagel@allete.com>

--- src/cbapi/response/models.py
+++ src/cbapi/response/models.py
@@ -254,8 +254,9 @@ class AlertQuery(Query):
 
         return None
 
-    def set_ignored(self, ignored_flag=True):
-        payload = {"updates": {"is_ignored": ignored_flag, "requested_status": "False Positive"}}
+    def set_ignored(self, ignored_flag=True, status="False Positive"):
+        """Ignore all future Alerts from the Report that triggered this Alert."""
+        payload = {"set_ignored": ignored_flag, "requested_status": status}
         return self._bulk_update(payload)
 
     def assign(self, target):
@@ -263,6 +264,9 @@ class AlertQuery(Query):
         return self._bulk_update(payload)
 
     def change_status(self, new_status):
+        allowed_statuses = ["In Progress", "Unresolved", "Resolved", "False Positive"]
+        if new_status not in allowed_statuses:
+            raise ApiError("Alert status must be one of {0}".format(allowed_statuses))
         payload = {"requested_status": new_status}
         return self._bulk_update(payload)
 
@@ -294,6 +298,25 @@ class Alert(MutableBaseModel):
             self._info = result[0]
         self._last_refresh_time = time.time()
         return True
+
+    def set_ignored(self, ignored_flag=True, status="False Positive"):
+        """Ignore all future Alerts from the Report that triggered this Alert."""
+        payload = {"set_ignored": ignored_flag, "requested_status": status}
+        payload["alert_ids"] = [self.unique_id]
+        return self._cb.post_object("/api/v1/alerts", payload)
+
+    def assign(self, target):
+        payload = {"assigned_to": target, "requested_status": "In Progress"}
+        payload["alert_ids"] = [self.unique_id]
+        return self._cb.post_object("/api/v1/alerts", payload)
+
+    def change_status(self, new_status):
+        allowed_statuses = ["In Progress", "Unresolved", "Resolved", "False Positive"]
+        if new_status not in allowed_statuses:
+            raise ApiError("Alert status must be one of {0}".format(allowed_statuses))
+        payload = {"status": new_status}
+        payload["unique_id"] = self.unique_id
+        return self._cb.post_object("/api/v1/alert/{0}".format(self.unique_id), payload)
 
     @property
     def process(self):
