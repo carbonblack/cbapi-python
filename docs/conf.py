28c03d39e84f8cfa08b8745a3378af43e5977a0a
Kylie Ebringer <kebringer@carbonblack.com>
2024-02-13 15:06:24 -0700

    fixing rtd build

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,9 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-# import sys (imports not needed)
-# import os
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
